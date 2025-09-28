f5a4512da39b02170cc2df2af82ebda034e29d1b
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-29 02:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function highlight() {
     //Write your code here
+	// let para = document.querySelector("p");
+	// para.addEventListener('mouseenter', () => {
+	// 	para.style.backgroundColor = "green";
+	// });
+	// para.addEventListener('mouseleave', () => {
+	//   para.style.backgroundColor = 'black';
+	// });
 
-
+	const bolds = document.querySelectorAll('strong');
+    
+    bolds.forEach(strong => {
+        strong.style.color = 'rgb(0, 128, 0)';
+    });
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const bolds = document.querySelectorAll('strong');
+	bolds.forEach(strong => {
+        strong.style.color = 'rgb(0, 0, 0)';
+    });
     
 }
